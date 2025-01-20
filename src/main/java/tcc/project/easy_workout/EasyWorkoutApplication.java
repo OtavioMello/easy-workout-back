489a0a5aad6eba45839b2f14fe9e3bdecdc6cb4b
otaviomello <otaviodemello15@gmail.com>
2025-01-20 19:00:48 -0300

    feat: created project base structure

---
+++ src/main/java/tcc/project/easy_workout/EasyWorkoutApplication.java
@@ -0,0 +1,13 @@
+package tcc.project.easy_workout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyWorkoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyWorkoutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tcc/project/easy_workout/EasyWorkoutApplicationTests.java
@@ -0,0 +1,13 @@
+package tcc.project.easy_workout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyWorkoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
