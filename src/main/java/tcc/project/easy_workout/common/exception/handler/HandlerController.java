04c9ab618de52d74b9ccb87b8899f9941142650c
otaviomello <otaviodemello15@gmail.com>
2025-03-17 16:30:28 -0300

    fix: fixed exception handler

--- src/main/java/tcc/project/easy_workout/common/exception/handler/HandlerController.java
+++ src/main/java/tcc/project/easy_workout/common/exception/handler/HandlerController.java
@@ -1,16 +1,13 @@
 package tcc.project.easy_workout.common.exception.handler;
 
 import com.auth0.jwt.exceptions.TokenExpiredException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import jakarta.ws.rs.*;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.CredentialsExpiredException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -20,8 +17,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import tcc.project.easy_workout.common.exception.model.ConflictException;
 import tcc.project.easy_workout.common.exception.model.CustomExceptionResponse;
 
-import javax.ws.rs.*;
-import java.io.IOException;
 import java.sql.SQLIntegrityConstraintViolationException;
 import java.time.Instant;
 
@@ -31,33 +26,6 @@ public class HandlerController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(HandlerController.class);
     private static final String LOG_TEMPLATE = "[Exception] An error occurred: status={}, message={}";
-    private final ObjectMapper objectMapper;
-
-    public void accessDeniedHandler(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException {
-        LOGGER.error(LOG_TEMPLATE, HttpStatus.FORBIDDEN, accessDeniedException.getMessage());
-        var customExceptionMessage = CustomExceptionResponse.builder()
-                .message(accessDeniedException.getMessage())
-                .details(request.getRequestURI())
-                .timestamp(Instant.now())
-                .build();
-
-        response.setStatus(HttpStatus.FORBIDDEN.value());
-        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-        response.getWriter().write(objectMapper.writeValueAsString(customExceptionMessage));
-    }
-
-    public void authenticationHandler(HttpServletRequest request, HttpServletResponse response, AuthenticationException authenticationException) throws IOException {
-        LOGGER.error(LOG_TEMPLATE, HttpStatus.FORBIDDEN, authenticationException.getMessage());
-        var customExceptionMessage = CustomExceptionResponse.builder()
-                .message(authenticationException.getMessage())
-                .details(request.getRequestURI())
-                .timestamp(Instant.now())
-                .build();
-
-        response.setStatus(HttpStatus.FORBIDDEN.value());
-        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-        response.getWriter().write(objectMapper.writeValueAsString(customExceptionMessage));
-    }
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler({NotFoundException.class})
